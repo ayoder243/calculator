8a702cb0e171f5edb5334c9e5949bd011ac739ed
Aaron Yoder <ayoder243@gmail.com>
2023-09-06 08:42:26 -0500

    Bad Commit Added too many things
    Made display box show which button is clicked
    ran operations as they were inputted

--- script.js
+++ script.js
@@ -30,6 +30,9 @@ function operate(x, o, y) {
       return multiply(x, y);
       break;
     case "/":
+      if(num2 == 0) {
+        return "no divide by 0 you caveperson"
+      }
       return divide(x, y);
       break;
     default:
@@ -37,3 +40,50 @@ function operate(x, o, y) {
       break;
   }
 }
+
+function populateDisplay(x) {
+  const displayBox = document.querySelector('#display');
+  displayBox.textContent = x;
+}
+
+function buttonClicks() {
+  buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      if(e.target.className == "num" && num1 == undefined) {
+        populateDisplay(e.target.id);
+        num1 = Number(e.target.id);
+      }
+      else if(e.target.className == "num" && num1 != undefined) {
+        populateDisplay(e.target.id);
+        num2 = Number(e.target.id);
+      }
+      else if(e.target.className == "operator" && num2 == undefined) {
+        populateDisplay(e.target.id);
+        operator = e.target.id;
+      }
+      else if(e.target.className == "operator" && num2 != undefined) {
+        populateDisplay(e.target.id);
+        num1 = operate(num1, operator, num2);
+        operator = e.target.id;
+        num2 = undefined;
+      }
+      else if(e.target.className == "equals") {
+        if(num1 != undefined && operator != undefined && num2 != undefined) {
+          populateDisplay(operate(num1, operator, num2));
+        }
+        else {
+          populateDisplay("ERROR")
+        }
+      }
+      else if(e.target.className == "clear") {
+        num1 = undefined;
+        num2 = undefined;
+        operator = undefined;
+        populateDisplay("")
+      }
+    })
+  });
+}
+
+buttonClicks();
